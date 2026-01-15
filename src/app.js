6d0173a882ce7f08590aa0d8f7c84247d4519384
Sparsh Bhardwaj <sparshbhardwaj321@gmail.com>
2026-01-15 12:34:49 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,31 @@ const express = require("express");
 
 const app = express();
 
+// playing with the routs
+// "?" ->value before it is optional
+// "+" -> value before can be added multiple times
+// "*" -> any thing can be entered in place of * eg "/a*b" -> /acdefb
+// we can aslo do chainig in this with () /a(bc)+d -> /abcbcbcbcd /abcd
+// can also use regex
+// "/a/" -> any thig can be written in place of a
+// "/*fly$" -> anything can be written in the start but must end's with fly
+
+// Now we will params and query
+
+// Query
+// app.get("/user", (req, res) => {
+//   const { user_id, password } = req.query;
+//   console.log(user_id, password);
+//   res.send("The query have been accepted");
+// });
+
+// Params
+app.get("/user/:userName/:userId/:password", (req, res) => {
+  const { userName, userId, password } = req.params;
+  console.log(userName, userId, password);
+  res.send("This is the params");
+});
+
 app.use("/api", (req, res) => {
   return res.json("This is the api route");
 });
